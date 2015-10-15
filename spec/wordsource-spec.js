5d92d4ef952ece882bfb28616db61197da10a5c5
SophGits <sophie.jiffy@gmail.com>
2015-10-15 22:12:44 +0100

    Complete most requirements.

--- spec/wordsource-spec.js
+++ spec/wordsource-spec.js
@@ -15,6 +15,20 @@ describe("Wordsource", function () {
     it("returns null", function () {
       expect(src.nextWord()).toBe(null);
     });
+
+  });
+
+  describe("When contains gte two items", function() {
+
+    beforeEach(function () {
+      src = new CommaSeparatedWordsource("lorem,ipsum,dolor");
+    });
+
+    it("returns the proceeding word", function() {
+      expect(src.nextWord()).toBe("lorem");
+      expect(src.nextWord()).toBe("ipsum");
+      expect(src.nextWord()).toBe("dolor");
+    });
   });
 
 });
--- wordsource.js
+++ wordsource.js
@@ -1,12 +1,54 @@
 "use strict";
 
 
-function CommaSeparatedWordsource() {
-
+function CommaSeparatedWordsource(words) {
+  if(words === ""){
+    this.words = [];
+  } else {
+    this.words = words.split(",");
+  }
+  this.seenWords =[];
 };
 
 CommaSeparatedWordsource.prototype.nextWord = function() {
-  return null;
+  if(this.words.length === 0) { return null; }
+  var seenWord = this.words.shift();
+  this.seenWords.push(seenWord);
+  return seenWord;
+};
+
+CommaSeparatedWordsource.prototype.count = function() {
+  return this.seenWords.length;
+};
+
+CommaSeparatedWordsource.prototype.topFiveWords = function() {
+  var quantities = this.seenWords.reduce(function(ret, val, i){
+    if(ret[val]) {
+      ret[val] += 1;
+    } else {
+      ret[val] = 1;
+    }
+    return ret;
+  }, {});
+
+  return Object.keys(quantities).sort(function(a,b) {
+    if(quantities[a] > quantities[b]) {
+      return 1;
+    }
+    if(quantities[a] < quantities[b]) {
+      return -1;
+    }
+    return 0;
+  }).reverse().slice(0,5);
+
+};
+
+CommaSeparatedWordsource.prototype.run = function() {
+  while(this.nextWord());
 };
 
 module.exports = CommaSeparatedWordsource;
+
+var src = new CommaSeparatedWordsource("lorem,ipsum,dolor,lorem,lorem,dolor,sit,sit,sit,sit,amet,consectetur,amet,hihi");
+src.run();
+console.log(src.topFiveWords());
